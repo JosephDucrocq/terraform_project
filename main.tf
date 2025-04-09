f770e194f5b573ad87fb3ea7a2f08f634ff23b0f
JosephDucrocq <ducrocq.joseph@gmail.com>
2025-04-09 15:13:52 +0100

    elastic bean

--- main.tf
+++ main.tf
@@ -22,3 +22,25 @@ resource "aws_ecr_repository" "my_repository" {
   name = "alydarecr"  # Name of the repository you want to create
 }
 
+resource "aws_elastic_beanstalk_application" "example_app" {
+  name        = "alydar-task-listing-app"
+  description = "Task listing app"
+}
+resource "aws_elastic_beanstalk_environment" "example_app_environment" {
+  name                = "alydar-task-listing-app-environment"
+  application         = aws_elastic_beanstalk_application.example_app.name
+  # This page lists the supported platforms
+  # we can use for this argument:
+  # https://docs.aws.amazon.com/elasticbeanstalk/latest/platforms/platforms-supported.html#platforms-supported.docker
+  solution_stack_name = "64bit Amazon Linux 2023 v4.0.1 running Docker"
+  setting {
+    namespace = "aws:autoscaling:launchconfiguration"
+    name      = "alydar_user"
+    value     = aws_iam_instance_profile.example_app_ec2_instance_profile.name
+  }
+  setting {
+    namespace = "aws:autoscaling:launchconfiguration"
+    name = "EC2KeyName"
+    value = "alydar_keypair"
+  }
+}
