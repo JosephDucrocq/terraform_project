aa85203469695d83d207f11b3df9591fcfef2dde
JosephDucrocq <ducrocq.joseph@gmail.com>
2025-04-11 11:40:41 +0100

    back to normal

--- infrastructure.tf
+++ infrastructure.tf
@@ -5,22 +5,21 @@ resource "aws_elastic_beanstalk_application" "example_app" {
   name        = "alydar-task-listing-app"
   description = "Task listing app"
 }
-resource "aws_elastic_beanstalk_environment" "example_app_environment" {
-    name                = "name-EB"
-    application         = aws_elastic_beanstalk_application.example_app.name
-    solution_stack_name = "64bit Amazon Linux 2023 v4.0.1 running Docker"
-  
-  setting {
-    namespace = "aws:autoscaling:launchconfiguration"
-    name      = "IamInstanceProfile"
-    value     = aws_iam_instance_profile.example_app_ec2_instance_profile.name
-}
-  setting {
-    namespace = "aws:autoscaling:launchconfiguration"
-    name      = "EC2KeyName"
-    value     = "alydar_terraform_key"
-  }
-}
+# resource "aws_elastic_beanstalk_environment" "example_app_environment" {
+#   name                = "name-EB"
+#   application         = aws_elastic_beanstalk_application.example_app.name
+#   solution_stack_name = "64bit Amazon Linux 2023 v4.0.1 running Docker"
+#   setting {
+#     namespace = "aws:autoscaling:launchconfiguration"
+#     name      = "IamInstanceProfile"
+#     value     = aws_iam_instance_profile.example_app_ec2_instance_profile.name
+#   }
+#   setting {
+#     namespace = "aws:autoscaling:launchconfiguration"
+#     name      = "EC2KeyName"
+#     value     = "alydar_terraform_key"
+#   }
+# }
 
 resource "aws_iam_instance_profile" "example_app_ec2_instance_profile" {
   name = "example_app_ec2_instance_profile"
