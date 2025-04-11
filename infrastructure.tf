32a825d843384d5d58c16d38ab9380c329373afd
JosephDucrocq <ducrocq.joseph@gmail.com>
2025-04-11 12:13:03 +0100

    no env

--- infrastructure.tf
+++ infrastructure.tf
@@ -100,7 +100,7 @@ resource "aws_secretsmanager_secret" "db_password" {
 resource "aws_secretsmanager_secret_version" "db_password_version" {
   secret_id     = aws_secretsmanager_secret.db_password.id
   secret_string = jsonencode({
-    password = "alydar123!"  # Replace with your actual password or variabl
+    password = "alydar123!"  # Replace with your actual password or variable
   })
 }
 
