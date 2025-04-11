81ce2c2cb5a42e64f0685fd3b21ed43063105b3a
JosephDucrocq <ducrocq.joseph@gmail.com>
2025-04-11 12:09:42 +0100

    no env

--- infrastructure.tf
+++ infrastructure.tf
@@ -100,10 +100,10 @@ resource "aws_secretsmanager_secret" "db_password" {
 resource "aws_secretsmanager_secret_version" "db_password_version" {
   secret_id     = aws_secretsmanager_secret.db_password.id
   secret_string = jsonencode({
-    password = "alydar123!"  # Replace with your actual password or variable
+    password = "alydar123!"  # Replace with your actual password or variabl
   })
 }
-  
+
 
 resource "aws_elastic_beanstalk_environment" "app_environment" {
   name                = "alydarEB"
